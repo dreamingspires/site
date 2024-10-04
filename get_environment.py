21df8e73f7aa922c0aa47aff1f326f03e5b0e243
mark-todd <markpeter.todd@hotmail.co.uk>
2024-10-04 14:12:42 +0100

    Reduced python script

--- get_environment.py
+++ get_environment.py
@@ -1,6 +1,4 @@
-import os
 
-# Load the .env file
 env_out: dict[str, str] = {}
 with open(".env") as f:
     for line in f:
@@ -9,21 +7,14 @@ with open(".env") as f:
             pass
         else:
             key, value = line.split("=", 1)
-
-            # Remove surrounding quotes from the value, if any
             value = value.strip().strip('"').strip("'")
-
-            # Set the environment variable
             env_out[key.strip()] = value
 
-# Read the config template
 with open('config.template.toml', 'r') as f:
     config = f.read()
 
 for k, v in env_out.items():
-    # Replace placeholders with actual environment variables
     config = config.replace("${" + k + "}", env_out[k])
 
-# Write the result to the actual config.toml
 with open('config.toml', 'w') as f:
     f.write(config)
